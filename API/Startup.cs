ac842c329062315ecfca0587e5c3b9ebdc4d17e4
kwarn <karlwarner.dev@gmail.com>
2022-10-03 12:17:38 +0100

    startup clean up

--- API/Startup.cs
+++ API/Startup.cs
@@ -4,6 +4,7 @@ using Persistence;
 using MediatR;
 using Application.Activities;
 using Application.Core;
+using API.Extensions;
 
 namespace API
 {
@@ -20,29 +21,8 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
-            services.AddCors(opt =>
-            {
-                opt.AddPolicy("CorsPolicy", policy =>
-                {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });
-            });
-            services.AddMediatR(typeof(List.Handler).Assembly);
-            services.AddMediatR(typeof(Create.Handler).Assembly);
-            services.AddMediatR(typeof(Edit.Handler).Assembly);
-            services.AddMediatR(typeof(Delete.Handler).Assembly);
-            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
-
+            services.AddApplicationServices(_config);           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
