c005a22b69b25d3008fc8895197cb4d6da6b624f
kwarn <karlwarner.dev@gmail.com>
2022-10-03 10:38:43 +0100

    Mediator initialization moved to BaseApiController

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,11 +1,14 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")] // whatever the controller class is called minus "controller"
-    public class BaseApiController: ControllerBase
+    public class BaseApiController : ControllerBase
     {
-        
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
     }
 }
